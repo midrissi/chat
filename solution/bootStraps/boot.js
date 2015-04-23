fa2702a04e2bbdd827657a2e1d1c351d68e52179
Mohamed IDRISSI <med.idrissi@outlook.com>
2015-04-23 16:31:13 +0100

    Simple chage

--- solution/bootStraps/boot.js
+++ solution/bootStraps/boot.js
@@ -2,7 +2,7 @@ directory.setLoginListener("loginListener", "administrator");
 require('publisher').startNotifs();
 
 currentSession().promoteWith("administrator");
-ds.Conversation.remove();
+//ds.Conversation.remove();
 if(ds.Person.length === 0){
 	ds.Person.fromArray(require('users'));
 }
